28a2674e3de8ba821260530064d45f2900a3ba77
lkaarnasre99 <165937094+lkaarnasre99@users.noreply.github.com>
2025-02-24 00:24:50 -0500

    error fix

--- main.tf
+++ main.tf
@@ -76,5 +76,8 @@ module "monitoring-alerts" {
    source = "./modules/monitoring"
    monitoring_project_id = var.project_id_1
    monitored_project_ids = [var.project_id_1, var.project_id_2] 
+   providers = {
+     google = google.project1 
+   }
   
 }
